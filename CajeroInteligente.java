f471993c8bba3d09db398eac203f44c9a94e9947
anavera011 <anaa011@hotmail.com>
2026-09-20 21:01:26 -0500

    Reto punto 10 completado

--- CajeroInteligente.java
+++ CajeroInteligente.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class CajeroInteligente {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int[] compras = { 12000, 25000, 8000, 35000, 15000 };
+        int[] compras = { 12000, 25000, 8000, 35000, 15000, 156000, 2000, 300000 };
 
         int cantidad_compras = 0;
         int total_ventas = 0;
         int compras_grandes = 0;
+        int compras_pequeñas = 0;
         for (int i = 0; i < compras.length; i++) {
             total_ventas = total_ventas + compras[i];
             cantidad_compras++;
-            if (compras[i] > 20000) {
+            if (compras[i] > 30000) {
                 compras_grandes++;
+            } else if (compras[i] <= 20000) {
+                compras_pequeñas++;
             }
         }
         double promedio = (double) total_ventas / cantidad_compras;
@@ -22,5 +25,11 @@ public class CajeroInteligente {
         System.out.println("Dinero recaudado: " + total_ventas);
         System.out.println("Compras superiores a $20.000: " + compras_grandes);
         System.out.println("El promedio compras es: " + promedio);
+
+        if (promedio > 25000) {
+            System.out.println("Día excelente");
+        } else {
+            System.out.println("Día normal");
+        }
     }
 }
